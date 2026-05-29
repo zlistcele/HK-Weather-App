efe581d15d749a140143b8bc9b62039ac2026390
zlistcele <zlistceleb@outlook.com>
2026-05-29 22:59:44 +0800

    Initial commit: Add main app entry point

---
+++ HKWeatherApp/HKWeatherAppApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct HKWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 400, minHeight: 300)
+        }
+    }
+}
